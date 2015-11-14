21bfefe163bff8d546b8dbb0e34dbc032c00c143
remo <remo.quadrelli@gmail.com>
2015-11-15 00:13:23 +0100

    reduce boards and rows

--- src/sudoku_solver/rules.cljs
+++ src/sudoku_solver/rules.cljs
@@ -1,24 +1,66 @@
 (ns ^:figwheel-always sudoku-solver.rules)
 
-(defn check-row [board v x y z dx dy]
+(defn check-row [board v x y dx dy]
   (not-any? true?
-          (for [i (range z)]
+          (for [i (range (count board))]
             (= (get-in board [(+ (* dy i) y)
                               (+ (* dx i) x)])
                v))))
 
-(defn check-row-horizontal [board v x y z]
-  (check-row board v x y z 1 0))
+(defn check-row-horizontal [board v x y]
+  (check-row board v x y 1 0))
 
-(defn check-row-vertical [board v x y z]
-  (check-row board v x y z 0 1))
+(defn check-row-vertical [board v x y]
+  (check-row board v x y 0 1))
 
-(defn check-square [board v x y z]
-  (let [r (.sqrt js/Math z)
+(defn check-square [board v x y]
+  (let [r (.sqrt js/Math (count board))
         qx (quot x r)
         qy (quot y r)]
     (not-any? true?
             (for [i (range (* qx r) (* (+ qx 1) r))
                   j (range (* qy r) (* (+ qy 1) r))]
-              (= (get-in board [i j])
+              (= (get-in board [j i])
                  v)))))
+
+(defn is-allowed? [board v x y]
+  (and (check-square board v x y)
+       (check-row-horizontal board v x y)
+       (check-row-vertical board v x y)))
+
+(defn get-allowed-values [board x y]
+  (filter #(is-allowed? board % x y) (range 1 (+ (count board) 1))))
+
+(defn is-solved? [board]
+  (every? true?
+          (for [y (range (count board))
+                x (range (count board))]
+            (> (get-in board [y x]) 0))))
+
+(defn reduce-row [board y row]
+  (reduce-kv
+    (fn [r x v]
+      (if (= 0 v)
+        (let [vs (get-allowed-values board x y)]
+          (if (= (count vs) 1)
+            (conj r (first vs))
+            (conj r 0)))
+        (conj r v)))
+   []
+   row))
+
+(defn reduce-board [board]
+  (reduce-kv
+    (fn [c i v]
+      (conj c (reduce-row board i v)))
+    []
+    board))
+
+(defn solve [board]
+  (if (is-solved? board)
+    (do (println "Solved" board) board)
+    (let [nb (reduce-board board)]
+      (println "Solve" board)
+      (if (= nb board)
+        (do (println "NOT  " board) board)
+        (solve nb)))))
--- test/sudoku_solver/rules_test.cljs
+++ test/sudoku_solver/rules_test.cljs
@@ -12,6 +12,12 @@
             [9 0 2 4 5 0 0 0 0]
             [5 0 6 1 0 0 0 0 0]])
 
+(deftest test-is-allowed
+  (is (r/is-allowed? board 3 1 0 9)))
+
+(deftest test-is-allowed-not-allowed
+  (is (not (r/is-allowed? board 2 1 0 9))))
+
 (deftest test-row-horizontal
   (is (r/check-row-horizontal board 3 1 0 9)))
 
@@ -35,3 +41,19 @@
 
 (deftest test-square-2nd-not-allowed
   (is (not (r/check-square board 4 5 5 9))))
+
+(deftest test-square-3nd
+  (is (r/check-square board 1 0 8 9)))
+
+(deftest test-square-3nd-not-allowed
+  (is (not (r/check-square board 4 8 0 9))))
+
+(deftest test-get-allowed-values
+  (is (= '(3 5 8) (r/get-allowed-values board 1 0 9))))
+
+(deftest test-is-solved-not
+  (is (not (r/is-solved? board))))
+
+(deftest test-solve
+  (is (= (r/solve board)
+        board)))
