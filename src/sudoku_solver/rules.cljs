95aba5fef024059790c64b7f0496dabdb03e8093
remo <remo.quadrelli@gmail.com>
2015-11-15 21:57:37 +0100

    check possible fields for values in sqare/row

--- src/sudoku_solver/rules.cljs
+++ src/sudoku_solver/rules.cljs
@@ -3,8 +3,8 @@
 (defn check-row [board v x y dx dy]
   (not-any? true?
           (for [i (range (count board))]
-            (= (get-in board [(+ (* dy i) y)
-                              (+ (* dx i) x)])
+            (= (get-in board [(+ (* dy i) (* dx y))
+                              (+ (* dx i) (* dy x))])
                v))))
 
 (defn check-row-horizontal [board v x y]
@@ -28,14 +28,52 @@
        (check-row-horizontal board v x y)
        (check-row-vertical board v x y)))
 
+(defn get-allowed-positions [board v x y dx dy]
+  (filter
+    #(not (nil? %))
+    (for [i (range (count board))]
+      (let [ny (+ (* dy i) (* dx y))
+            nx (+ (* dx i) (* dy x))]
+          (when (and (= 0 (get-in board [ny nx]))
+                     (is-allowed? board v nx ny))
+            [ny nx])))))
+
+(defn get-allowed-positions-horizontal [board v x y]
+  (get-allowed-positions board v x y 1 0))
+
+(defn get-allowed-positions-vertical [board v x y]
+  (get-allowed-positions board v x y 0 1))
+
+(defn get-allowed-positions-square [board v x y]
+  (filter
+    #(not (nil? %))
+    (let [r (.sqrt js/Math (count board))
+          qx (quot x r)
+          qy (quot y r)]
+        (for [nx (range (* qx r) (* (+ qx 1) r))
+              ny (range (* qy r) (* (+ qy 1) r))]
+            (when (and (= 0 (get-in board [ny nx]))
+                       (is-allowed? board v nx ny))
+                 [ny nx])))))
+
 (defn get-allowed-values [board x y]
   (filter #(is-allowed? board % x y) (range 1 (+ (count board) 1))))
 
-(defn is-solved? [board]
-  (every? true?
-          (for [y (range (count board))
-                x (range (count board))]
-            (> (get-in board [y x]) 0))))
+(defn only-possible-position? [board v x y]
+  (some #(and (= (count %) 1)
+              (= (first %) [y x]))
+    (vector (get-allowed-positions-square board v x y)
+            (get-allowed-positions-vertical board v x y)
+            (get-allowed-positions-horizontal board v x y))))
+
+(defn get-only-possible-value [board vs x y]
+  (let [v (filter
+            #(only-possible-position? board % x y)
+            vs)]
+    ;(println y " " x " " v)
+    (if (seq v)
+        (first v)
+        0)))
 
 (defn reduce-row [board y row]
   (reduce-kv
@@ -44,7 +82,7 @@
         (let [vs (get-allowed-values board x y)]
           (if (= (count vs) 1)
             (conj r (first vs))
-            (conj r 0)))
+            (conj r (get-only-possible-value board vs x y))))
         (conj r v)))
    []
    row))
@@ -56,11 +94,16 @@
     []
     board))
 
+(defn is-solved? [board]
+  (every? true?
+          (for [y (range (count board))
+                x (range (count board))]
+            (> (get-in board [y x]) 0))))
+
 (defn solve [board]
   (if (is-solved? board)
-    (do (println "Solved" board) board)
+    board
     (let [nb (reduce-board board)]
-      (println "Solve" board)
       (if (= nb board)
-        (do (println "NOT  " board) board)
+        board
         (solve nb)))))
--- test/sudoku_solver/rules_test.cljs
+++ test/sudoku_solver/rules_test.cljs
@@ -12,48 +12,94 @@
             [9 0 2 4 5 0 0 0 0]
             [5 0 6 1 0 0 0 0 0]])
 
+(def solved-board [[1 3 5 6 8 9 2 7 4]
+                   [2 4 8 5 1 7 6 9 3]
+                   [7 6 9 3 2 4 1 8 5]
+                   [4 9 3 2 6 5 7 1 8]
+                   [8 5 7 9 4 1 3 2 6]
+                   [6 2 1 7 3 8 4 5 9]
+                   [3 7 4 8 9 2 5 6 1]
+                   [9 1 2 4 5 6 8 3 7]
+                   [5 8 6 1 7 3 9 4 2]])
+
 (deftest test-is-allowed
-  (is (r/is-allowed? board 3 1 0 9)))
+  (is (r/is-allowed? board 3 1 0)))
 
 (deftest test-is-allowed-not-allowed
-  (is (not (r/is-allowed? board 2 1 0 9))))
+  (is (not (r/is-allowed? board 2 1 0))))
 
 (deftest test-row-horizontal
-  (is (r/check-row-horizontal board 3 1 0 9)))
+  (is (r/check-row-horizontal board 1 6 2)))
 
 (deftest test-row-horizontal-not-allowed
-  (is (not (r/check-row-horizontal board 2 1 0 9))))
+  (is (not (r/check-row-horizontal board 2 1 0))))
 
 (deftest test-row-vertical
-  (is (r/check-row-vertical board 3 1 0 9)))
+  (is (r/check-row-vertical board 3 1 0)))
 
 (deftest test-row-vertical-not-allowed
-  (is (not (r/check-row-vertical board 2 1 0 9))))
+  (is (not (r/check-row-vertical board 2 1 0))))
 
 (deftest test-square
-  (is (r/check-square board 2 1 0 9)))
+  (is (r/check-square board 1 7 0)))
 
 (deftest test-square-not-allowed
-  (is (not (r/check-square board 7 1 0 9))))
+  (is (not (r/check-square board 7 1 0))))
 
 (deftest test-square-2nd
-  (is (r/check-square board 2 5 5 9)))
+  (is (r/check-square board 2 5 5)))
 
 (deftest test-square-2nd-not-allowed
-  (is (not (r/check-square board 4 5 5 9))))
+  (is (not (r/check-square board 4 5 5))))
 
 (deftest test-square-3nd
-  (is (r/check-square board 1 0 8 9)))
+  (is (r/check-square board 1 0 8)))
 
 (deftest test-square-3nd-not-allowed
-  (is (not (r/check-square board 4 8 0 9))))
+  (is (not (r/check-square board 4 8 0))))
 
 (deftest test-get-allowed-values
-  (is (= '(3 5 8) (r/get-allowed-values board 1 0 9))))
+  (is (= '(3 5 8) (r/get-allowed-values board 1 0))))
+
+(deftest test-get-allowed-positions-horizontal
+  (is (= '([0 1] [0 2] [0 3]) (r/get-allowed-positions-horizontal board 5 1 0))))
+
+(deftest test-get-allowed-positions-vertical
+  (is (= '([0 1] [1 1] [3 1] [4 1]) (r/get-allowed-positions-vertical board 5 1 0))))
+
+(deftest test-get-allowed-positions-square
+  (is (= '([0 1] [1 1] [0 2] [1 2]) (r/get-allowed-positions-square board 5 1 0))))
+
+(deftest test-get-allowed-positions-horizontal-2nd
+  (is (= '([2 6]) (r/get-allowed-positions-horizontal board 1 6 2))))
+
+(deftest test-get-allowed-positions-vertical-2nd
+  (is (= '([2 6] (r/get-allowed-positions-vertical board 1 6 2)))))
+
+(deftest test-get-allowed-positions-square-2nd
+  (is (= '([2 6]) (r/get-allowed-positions-square board 1 6 2))))
+
+(deftest test-get-allowed-positions-horizontal-3nd
+  (is (= '([2 2] [2 6]) (r/get-allowed-positions-horizontal board 9 6 2))))
+
+(deftest test-get-allowed-positions-vertical-3nd
+  (is (= '([2 6] [3 6] [5 6] [8 6]) (r/get-allowed-positions-vertical board 9 6 2))))
+
+(deftest test-get-allowed-positions-square-3nd
+  (is (= '([2 6] [1 7]) (r/get-allowed-positions-square board 9 6 2))))
+
+(deftest test-only-possible-position
+  (is (r/only-possible-position? board 1 6 2)))
+
+(deftest test-only-possible-position-not
+  (is (not (r/only-possible-position? board 5 0 1))))
+
+(deftest test-is-solved?
+  (is (r/is-solved? solved-board)))
 
 (deftest test-is-solved-not
   (is (not (r/is-solved? board))))
 
 (deftest test-solve
   (is (= (r/solve board)
-        board)))
+        solved-board)))
